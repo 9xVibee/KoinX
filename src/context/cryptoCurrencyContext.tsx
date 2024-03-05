d41098e4e097a29555d3a16eba1750e6e41aa13c
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-05 22:18:55 +0530

    chore: Initial commit

---
+++ src/context/cryptoCurrencyContext.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
+
+interface CryptoCurrencyContextType {
+  cryptoCurrency: string;
+  setCryptoCurrency: Dispatch<SetStateAction<string>>;
+}
+
+export const cryptoCurrencyContext = createContext<CryptoCurrencyContextType>({
+  cryptoCurrency: "",
+  setCryptoCurrency: () => {},
+});
+
+const CryptocurrencyContext = ({ children }: { children: React.ReactNode }) => {
+  const [cryptoCurrency, setCryptoCurrency] = useState<string>("bitcoin");
+
+  return (
+    <cryptoCurrencyContext.Provider
+      value={{
+        cryptoCurrency,
+        setCryptoCurrency,
+      }}
+    >
+      {children}
+    </cryptoCurrencyContext.Provider>
+  );
+};
+
+export default CryptocurrencyContext;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CryptocurrencyContext from "./context/cryptoCurrencyContext";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CryptocurrencyContext>
+      <App />
+    </CryptocurrencyContext>
+  </React.StrictMode>
+);
