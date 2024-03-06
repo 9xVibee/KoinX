bd931664707f2c0fcbcc743e3d57bbe750502644
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-06 08:30:27 +0530

    feat: Navbar added into the App component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <p className="cursor-pointer">Crypto Taxes</p>
         <p className="cursor-pointer">Free Tools</p>
         <p className="cursor-pointer">Resources Center</p>
-        <button className="px-4 py-2 bg-gradient-to-r from-blue-500 to-blue-700 rounded-md text-white hover:opacity-80 transition">
+        <button className="px-5 py-1.5 bg-gradient-to-r from-blue-500 to-blue-700 rounded-md text-white hover:opacity-80 transition">
           Get Started
         </button>
       </div>
